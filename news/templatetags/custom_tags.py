aa783bc541932db162ea28b1248e6d28540e2f15
John Idogun <Sirneij@gmail.com>
2021-09-25 10:41:01 +0100

    Updated custom_tags.py

--- news/templatetags/custom_tags.py
+++ news/templatetags/custom_tags.py
@@ -1,1 +1,10 @@
 from news.models import LatestStory, Comment
+from django import template
+
+register = template.Library()
+
+@register.filter
+def story_title(id):
+    comm = Comment.objects.select_related('story').get(id=id)
+    if comm:
+        return comm.story.title
