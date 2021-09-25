f33c5be78e322219609457b79c99213a4f3ce320
Idogun John <sirneij@gmail.com>
2021-09-25 21:32:55 +0100

    Added some urls

--- news/templatetags/custom_tags.py
+++ news/templatetags/custom_tags.py
@@ -15,3 +15,16 @@ def story_title(parent_id):
         return comm.title
     else:
         return "Parent story has not been fetched yet"
+
+
+@register.filter
+def story_url(parent_id):
+    comm = LatestStory.objects.filter(unique_api_story_id=parent_id).first()
+    if comm:
+        if comm.story_type == "comment":
+            comm2 = LatestStory.objects.filter(unique_api_story_id=comm.parent_id).first()
+            if comm2:
+                return comm2.get_absolute_url()
+        return comm.get_absolute_url()
+    else:
+        return "#"
